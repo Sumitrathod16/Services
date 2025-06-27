0bdda176f35ca8ad86ae857bbdac33c4f3b5e05c
Sumitrathod16 <sr8705589@gmail.com>
2025-06-27 17:05:58 +0000

    login css

--- myapp/src/components/Home.js
+++ myapp/src/components/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Home.css';
 function Home() {
   return (
-    <div>
+    <div className='home-container'>
       <h2>Welcome to the Home Page!</h2>
       <p>This is the main landing page of your React app.</p>
     </div>
