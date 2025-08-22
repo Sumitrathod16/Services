42d4b3f7194af613bb8c9aa0ee0215b7b9feb846
Sumitrathod16 <sr8705589@gmail.com>
2025-08-22 16:31:48 +0530

    .

--- myapp/src/components/Flutter.js
+++ myapp/src/components/Flutter.js
@@ -1,125 +1,120 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 const syllabusData = [
   {
     id: 1,
-    title: 'Introduction to Flutter',
-    description: '',
-    details: 'This section covers the foundations of deep learning, including definitions, types, and how it’s used in modern applications.',
+    title: "Introduction to Flutter",
+    description: "",
+    details:
+      "This section covers the foundations of deep learning, including definitions, types, and how it’s used in modern applications.",
 
     chapters: [
-        "Learn to setup new material app using android studio",
-    "Overview of Flutter",
-    "Basic Dart Programming concepts",
-    "Understanding the widgets",
-    "Displaying the content on screen",
-    "Debugging and testing app with flutter"
+      "Learn to setup new material app using android studio",
+      "Overview of Flutter",
+      "Basic Dart Programming concepts",
+      "Understanding the widgets",
+      "Displaying the content on screen",
+      "Debugging and testing app with flutter",
     ],
-    notes:"notes.pdf"
+    notes: "notes.pdf",
   },
   {
     id: 2,
-    title: 'Stateful and stateless widdgets',
-    description: 'Understanding the architecture and function of ANNs',
-    details: 'Here, we dive into perceptrons, activation functions, and forward/backward propagation.',
-    
-    chapters: [
-       "Stateless vs Stateful widgets",
-    "Define State",
-    "The setState() method"
+    title: "Stateful and stateless widdgets",
+    description: "Understanding the architecture and function of ANNs",
+    details:
+      "Here, we dive into perceptrons, activation functions, and forward/backward propagation.",
 
+    chapters: [
+      "Stateless vs Stateful widgets",
+      "Define State",
+      "The setState() method",
     ],
-    notes:"notes.pdf"
+    notes: "notes.pdf",
   },
   {
     id: 3,
-    title: 'Forms',
-    description: '',
-    details: 'This module explains how deep architectures differ and how to train them effectively.',
-    
+    title: "Forms",
+    description: "",
+    details:
+      "This module explains how deep architectures differ and how to train them effectively.",
+
     chapters: [
       "Creating login and signup forms",
-   "Handling User input",
-   "Form Validation",
-   "Submitting form and retrieve data"
-
+      "Handling User input",
+      "Form Validation",
+      "Submitting form and retrieve data",
     ],
-    notes:"notes.pdf"
+    notes: "notes.pdf",
   },
   {
     id: 4,
-    title: 'Widgets',
-    description: 'Latest advancements and future directions of the field',
-    details: 'You’ll learn about transformers, foundation models, and cutting-edge applications.',
-  
-    chapters: ["Creating login and signup forms",
-   "Handling User input",
-   "Form Validation",
-   "Submitting form and retrieve data"
+    title: "Widgets",
+    description: "Latest advancements and future directions of the field",
+    details:
+      "You’ll learn about transformers, foundation models, and cutting-edge applications.",
 
+    chapters: [
+      "Creating login and signup forms",
+      "Handling User input",
+      "Form Validation",
+      "Submitting form and retrieve data",
     ],
-    notes:"notes.pdf"
+    notes: "notes.pdf",
   },
   {
     id: 5,
-    title: 'User Interface',
-    description: 'Latest advancements and future directions of the field',
-    details: 'You’ll learn about transformers, foundation models, and cutting-edge applications.',
-  
-    chapters: [
-      "Applying theme data",
-      "Applying custom font"
-    
+    title: "User Interface",
+    description: "Latest advancements and future directions of the field",
+    details:
+      "You’ll learn about transformers, foundation models, and cutting-edge applications.",
 
-    ],
-    notes:"notes.pdf"
+    chapters: ["Applying theme data", "Applying custom font"],
+    notes: "notes.pdf",
   },
   {
     id: 6,
-    title: 'Asynchronous Function',
-    description: 'Latest advancements and future directions of the field',
-    details: 'You’ll learn about transformers, foundation models, and cutting-edge applications.',
-    
-    chapters: [
-     "Future Function",
-      "Async and Await"
-    ],
-    notes:"notes.pdf"
+    title: "Asynchronous Function",
+    description: "Latest advancements and future directions of the field",
+    details:
+      "You’ll learn about transformers, foundation models, and cutting-edge applications.",
+
+    chapters: ["Future Function", "Async and Await"],
+    notes: "notes.pdf",
   },
   {
     id: 7,
-    title: 'Working with the App data',
-    description: 'Latest advancements and future directions of the field',
-    details: 'You’ll learn about transformers, foundation models, and cutting-edge applications.',
+    title: "Working with the App data",
+    description: "Latest advancements and future directions of the field",
+    details:
+      "You’ll learn about transformers, foundation models, and cutting-edge applications.",
 
     chapters: [
-   "Http Package",
+      "Http Package",
       "Model class and json Parsing",
-      "Displaying API data"
+      "Displaying API data",
     ],
-    notes:"notes.pdf"
+    notes: "notes.pdf",
   },
   {
     id: 8,
-    title: 'Navigation',
-    description: 'Latest advancements and future directions of the field',
-    details: 'You’ll learn about transformers, foundation models, and cutting-edge applications.',
-    
-    chapters: [
-      "Navigator and Routes",
-      "Applying push() and pop()"
+    title: "Navigation",
+    description: "Latest advancements and future directions of the field",
+    details:
+      "You’ll learn about transformers, foundation models, and cutting-edge applications.",
 
-    ],
-    notes:"notes.pdf"
+    chapters: ["Navigator and Routes", "Applying push() and pop()"],
+    notes: "notes.pdf",
   },
   {
     id: 9,
-    title: 'Container',
-    description: 'Latest advancements and future directions of the field',
-    details: 'You’ll learn about transformers, foundation models, and cutting-edge applications.',
-    
+    title: "Container",
+    description: "Latest advancements and future directions of the field",
+    details:
+      "You’ll learn about transformers, foundation models, and cutting-edge applications.",
+
     chapters: [
-       "Container",
+      "Container",
       "Card",
       "Importing images from network",
       "Importing images as assets",
@@ -127,52 +122,51 @@ const syllabusData = [
       "Listview and ListTile",
       "GridView",
       "Alert dialogbox",
-      "Building views using listview.builder"
-
-
+      "Building views using listview.builder",
     ],
-    notes:"notes.pdf"
+    notes: "notes.pdf",
   },
-   {
-    id: 10,
-    title: 'Offline Storage',
-    description: 'Latest advancements and future directions of the field',
-    details: 'You’ll learn about transformers, foundation models, and cutting-edge applications.',
-    chapters: [
-     "Basic concept of sqlite database",
-     "Database Connection",
-     "CRUD operation on flutter database"
-],
-    notes:"notes.pdf"
-  },
-   {
-    id: 11,
-    title: 'Basic GIT',
-    description: 'Latest advancements and future directions of the field',
-    details: 'You’ll learn about transformers, foundation models, and cutting-edge applications.',
-  
-    chapters: [
-       
-],
-    notes:"notes.pdf"
-  }, 
   {
-    id:12,
-    title: 'Sources',
-    description: 'Additional resources for further learning',
-    details: 'Links to online courses, books, and articles for deeper understanding.',
-    chapters:[
+    id: 10,
+    title: "Offline Storage",
+    description: "Latest advancements and future directions of the field",
+    details:
+      "You’ll learn about transformers, foundation models, and cutting-edge applications.",
+    chapters: [
+      "Basic concept of sqlite database",
+      "Database Connection",
+      "CRUD operation on flutter database",
+    ],
+    notes: "notes.pdf",
+  },
+  {
+    id: 11,
+    title: "Basic GIT",
+    description: "Latest advancements and future directions of the field",
+    details:
+      "You’ll learn about transformers, foundation models, and cutting-edge applications.",
+
+    chapters: [],
+    notes: "notes.pdf",
+  },
+  {
+    id: 12,
+    title: "Sources",
+    description: "Additional resources for further learning",
+    details:
+      "Links to online courses, books, and articles for deeper understanding.",
+    chapters: [
       "Online Courses: Coursera, edX, Udacity",
       "Flutter Documentation: https://flutter.dev/docs",
       "Flutter YouTube Channel: https://www.youtube.com/c/flutterdev",
-      "Flutter Community: https://flutter.dev/community"
+      "Flutter Community: https://flutter.dev/community",
     ],
-    url:[
+    url: [
       "https://flutter.dev/docs",
       "https://www.youtube.com/c/flutterdev",
-      "https://flutter.dev/community"
-    ]
-  }
+      "https://flutter.dev/community",
+    ],
+  },
 ];
 
 const Syllabus = () => {
@@ -292,9 +286,12 @@ const Syllabus = () => {
         <aside className="sidebar">
           <h2>Syllabus</h2>
           <ul>
-            <li><Link to="/Flutter">Syllabus</Link></li>
-            <li><Link to="/Flutterassign">Assignment</Link></li>
-            
+            <li>
+              <Link to="/Flutter">Syllabus</Link>
+            </li>
+            <li>
+              <Link to="/Flutterassign">Assignment</Link>
+            </li>
           </ul>
         </aside>
 
@@ -307,7 +304,9 @@ const Syllabus = () => {
             >
               <div className="icon">{item.icon}</div>
               <div className="info">
-                <h3>{item.id}. {item.title}</h3>
+                <h3>
+                  {item.id}. {item.title}
+                </h3>
                 <p>{item.description}</p>
                 {item.id === 1 && (
                   <div className="progress-bar">
@@ -322,41 +321,47 @@ const Syllabus = () => {
                         <li key={idx}>{chapter}</li>
                       ))}
                     </ul>
-                     {item.notes && (
+                    {item.notes && (
                       <div style={{ marginTop: "10px" }}>
                         <a
                           href={item.notes}
                           target="_blank"
                           rel="noopener noreferrer"
-                          style={{ color: "#4f46e5", textDecoration: "underline" }}
+                          style={{
+                            color: "#4f46e5",
+                            textDecoration: "underline",
+                          }}
                         >
                           Download Notes
                         </a>
+                      </div>
+                    )}
+                    {item.url && Array.isArray(item.url) && (
+                      <div style={{ marginLeft: "20px", marginTop: "10px" }}>
+                        <strong>Useful Links:</strong>
+                        <ul>
+                          {item.url.map((link, index) => (
+                            <li key={index}>
+                              <a
+                                href={link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                style={{
+                                  color: "#4f46e5",
+                                  textDecoration: "none",
+                                }}
+                              >
+                                {link}
+                              </a>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    )}
                   </div>
                 )}
-                {item.url && Array.isArray(item.url) && (
-                  <div style={{marginLeft:"20px", marginTop: "10px" }}>
-                    <strong>Useful Links:</strong>
-                    <ul>
-                    {item.url.map((link, index) => (
-                      <li key={index}>
-                        <a
-                          href={link}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          style={{ color: "#4f46e5", textDecoration: "none" }}
-                        >
-                          {link}
-                        </a>
-                      </li>
-                    ))}
-                 </ul>
               </div>
-                )}
-                </div>
-                )}
-                </div>
-              </div>
+            </div>
           ))}
         </section>
       </div>
